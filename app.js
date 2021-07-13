56f450a3734ae6df467be9e35064e086b223636d
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-13 16:59:59 -0500

    set up basic app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-import express from 'express';
+const express = require('express');
+const app = express();
+
+// Set port
+const port = process.env.PORT || 3000;
+
+// Routing
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the Server!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
