dadb9d9a0b61974c5bb5c0f44d3381faf17795ac
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-13 17:52:01 -0500

    sets up api to get tours

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 // Set port
 const port = process.env.PORT || 3000;
 
+// Get data
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8')
+);
+
 // Routing
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the Server!');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 // Start server
