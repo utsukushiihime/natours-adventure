045887fadb01fcb5f6d4b19464114f83faede30e
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-13 23:20:50 -0500

    adds routes post route for api

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 // Set port
 const port = process.env.PORT || 3000;
 
@@ -14,10 +17,48 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: '404 - Invalid ID',
+    });
+    return;
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
 
 // Start server
