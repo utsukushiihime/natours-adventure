7fac80e376f495f775e864a36b43e71571b56ff1
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-14 20:58:14 -0500

    adds env

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 
 // Import routes
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const app = express();
+
 // Middleware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
 const app = require('./app');
 
+// console.log(process.env);
+
 // Set port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Start server
 app.listen(port, () => {
