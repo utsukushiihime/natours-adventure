289c733f0d103e74ef62f6515b146bfddeb56275
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-15 16:46:50 -0500

    adds more data to tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,9 +5,31 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour name is required'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Max group size is required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is required'],
+  },
+  duration: { type: Number, required: [true, 'Duration is required'] },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: { type: Number, required: [true, 'Price is required'] },
+  priceDiscount: { type: Number },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Summary is required'],
+  },
+  description: { type: String, trim: true },
+  imageCover: { type: String, required: [true, 'Image cover is required'] },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
