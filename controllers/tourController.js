69319a84664562a78e7e3cfae6710ab6b62fbd86
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-15 19:41:03 -0500

    filters get all tours in controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,22 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+
+    console.log(req.query, queryObj);
+
+    const tours = await Tour.find(queryObj);
+
+    /* NOTE query using mongoose methods */
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
 
     res.status(200).json({
       status: 'success',
