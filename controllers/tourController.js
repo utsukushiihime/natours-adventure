36ea6b0934a5510c0cc2c59b1c03e9a208aeb45a
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-15 20:42:16 -0500

    adds sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,15 +2,31 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => {
       delete queryObj[el];
     });
 
-    console.log(req.query, queryObj);
-    const tours = await Tour.find(queryObj);
+    // Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
 
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      // sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Execute query with advanced filtering
+    const tours = await query;
+
+    // Send response
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
