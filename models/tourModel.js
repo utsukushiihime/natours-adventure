2e8ff52afbd960aaa837e6a06d758ee760d36ab4
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-18 18:55:34 -0500

    adds slugs to tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -31,6 +32,7 @@ const tourSchema = new mongoose.Schema(
     images: [String],
     createdAt: { type: Date, default: Date.now(), select: false },
     startDates: [Date],
+    slug: String,
   },
   {
     toJSON: { virtuals: true },
@@ -40,6 +42,14 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  if (this.isNew) {
+    this.slug = slugify(this.name, { lower: true });
+  }
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
